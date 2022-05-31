22a9f91d4483b5e8e5b5ec89bb217e1a12f8ca16
Diego Lacerda <diego.lacerda.alves@gmail.com>
2022-05-31 09:11:28 -0300

    schema e transacionando dados + o basico de queries e snapshots

--- src/ecommerce/core.clj
+++ src/ecommerce/core.clj
@@ -1,20 +1,31 @@
 (ns ecommerce.core
   (:use clojure.pprint)
   (:require [datomic.api :as d]
-            [ecommerce.db :as db]))
+            [ecommerce.db :as db]
+            [ecommerce.model :as model]))
 ;iniciar datomic
 ;cd /Users/diego.lacerda/Documents/softwares/datomic-pro-0.9.5951
 ;bin/transactor config/dev-transactor-template.properties
 
 
 (def conn (db/abre-conexao))
-(pprint conn)
 
-; Produtos
-; id?
-; nome String 1 ==> Computador Novo
-; slug String 1 ==> /computador_novo
-; preco ponto flutuantes 1 ==> 3500.10
-; 17 nome Telefone Caro
-; 17 slug /telefone
-; 17 preco 8888.88
+(db/cria-schema conn)
+
+(let [computador (model/novo-produto "Computador Novo", "/computador_novo", 2500.10M)]
+  (d/transact conn [computador]))
+
+;o banco no instante que executou a linha
+(def db (d/db conn))
+
+(d/q '[:find ?entidade
+       :where [?entidade :produto/nome]] db)
+
+(let [celular (model/novo-produto "Celular Caro", "/celular", 888888.10M)]
+  (d/transact conn [celular]))
+
+; tirando uma nova fotografia (SNAPSHOT) do banco
+(def db (d/db conn))
+
+(d/q '[:find ?entidade
+       :where [?entidade :produto/nome]] db)
